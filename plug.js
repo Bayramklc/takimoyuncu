e88a29f2cb473729171ce30415ee748ffba4e99e
Bayramklc <byramklcc@gmail.com>
2014-09-17 18:07:03 +0300

    Update plug.js

--- plug.js
+++ plug.js
@@ -1,6 +1,6 @@
 javascript:(function(){$.getScript('https://raw.githubusercontent.com/Bayramklc/takimoyuncu/master/Turkishlang.js');}());
 
-javascript:(function(){$('.room-background').css('background-image', 'url(http://i.imgur.com/EFXFnql.png)');}());
+javascript:(function(){$('.room-background').css('background-image', 'url(http://i.imgur.com/XjiQctM.png)');}());
 
 javascript:(function(){$.getScript('https://fungustime.pw/tastyplug/tastyplug.js');}());
 
